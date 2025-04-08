ad4d6248fb6cc9169d5209fcc73277a9795a7eca
denizberkin <berkindeniz2000@gmail.com>
2025-04-08 23:35:25 +0300

    add training with model and results save mechanism

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import os
 
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
+import torch
+import pandas as pd
 
 from utils.preprocessing import set_seed, get_retinal_transforms
 from utils.dataset import get_loaders
 from utils.optim import optimizer_setup
+from utils.loss import BCEDiceLoss
 from utils.swin_res_unet import SwinResUNet
 from utils.unet import UNet
+from src.train import train_model
 
 
 if __name__ == "__main__":
@@ -19,6 +21,7 @@ if __name__ == "__main__":
     BATCH_SIZE = 4
     LR = 1.e-4
     NUM_EPOCHS = 40
+    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
     
     set_seed(42)  # set seed for reproducibility
 
@@ -35,9 +38,30 @@ if __name__ == "__main__":
         )
     
     # Change model here
-    model = SwinResUNet()
-    optimizer_setup(model=model, num_epochs=NUM_EPOCHS)  # setup optimizer and scheduler
+    model = SwinResUNet().to(DEVICE)
+    optimizer, scheduler = optimizer_setup(model=model, num_epochs=NUM_EPOCHS)
+    criterion = BCEDiceLoss().to(DEVICE)
     
+    # train the model
+    model, val_results = train_model(
+        model=model,
+        train_loader=dl_train,
+        val_loader=dl_test,
+        optimizer=optimizer,
+        scheduler=scheduler,
+        criterion=criterion,
+        num_epochs=NUM_EPOCHS,
+        device=DEVICE
+        )
     
+    # Save the model
+    model_save_path = os.path.join(EXP_BASE_DIR, EXP_NAME, f"{str(model)}_model.pth")
+    torch.save(model.state_dict(), model_save_path)
+    print(f"Model saved to {model_save_path}")
     
+    # Save the training results
+    results_save_path = os.path.join(EXP_BASE_DIR, EXP_NAME, f"{str(model)}_results.csv")
+    val_results_df = pd.DataFrame(val_results)  # key, list of values
+    val_results_df.to_csv(results_save_path, index=False)
+    print(f"Val results saved to {results_save_path}")
     
